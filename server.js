528e4f138142da9d9218a65bd1e95c3b53490804
priyanka <priyankaverma178@gmail.com>
2016-10-11 08:32:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article_one',function(req,res){
 });
 
 app.get('/article_two',function(req,res){
-   res.send('article two is requested and will be respond'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article_two.html')); 
 });
 
 app.get('/article_three',function(req,res){
